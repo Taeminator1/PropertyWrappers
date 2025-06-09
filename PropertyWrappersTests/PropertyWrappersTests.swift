bb636a1938f51bf72faba67974d679bfd2a8ccfc
TAEMIN YUN <dbsxoals689@naver.com>
2025-06-09 09:23:57 +0900

    Initial Commit

---
+++ PropertyWrappersTests/PropertyWrappersTests.swift
@@ -0,0 +1,17 @@
+//
+//  PropertyWrappersTests.swift
+//  PropertyWrappersTests
+//
+//  Created by Taemin Yun on 6/9/25.
+//
+
+import Testing
+@testable import PropertyWrappers
+
+struct PropertyWrappersTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
